b264cbc7456be78ee9ea961a1faf723b830b86b9
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2025-11-09 00:42:11 -0800

    [zsh] Support omz plugins and starship prompt

--- private_dot_config/zsh/plugins.zsh
+++ private_dot_config/zsh/plugins.zsh
@@ -2,3 +2,9 @@ plug "zsh-users/zsh-syntax-highlighting"
 plug "zsh-users/zsh-autosuggestions"
 
 plug "zap-zsh/supercharge"
+plug "zap-zsh/exa"
+
+plug "$HOME/.config/zsh-plugins/plugins/aliases"
+plug "$HOME/.config/zsh-plugins/plugins/brew"
+plug "$HOME/.config/zsh-plugins/plugins/direnv"
+plug "$HOME/.config/zsh-plugins/plugins/git"
