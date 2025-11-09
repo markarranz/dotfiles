2c7d13f77dc6d74438b2bd587892a6fca3d7b331
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2025-11-09 11:25:46 -0800

    [zsh] Move omz plugins directory inside zsh directory

--- private_dot_config/zsh/README.md
+++ private_dot_config/zsh/README.md
@@ -6,3 +6,10 @@ Add the following to the `/etc/zshenv` file:
 export XDG_CONFIG_HOME="$HOME/.config"
 export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
 ```
+
+## OMZ Plugin Support
+
+To add oh-my-zsh plugins:
+
+1. add the plugin name to the `OMZ_PLUGINS` array in [`plugins.zsh`](./plugins.zsh)
+1. add the plugin name to the `includes` array in [`.chezmoiexternal.toml`](../../.chezmoiexternal.toml.tmpl)
--- private_dot_config/zsh/plugins.zsh
+++ private_dot_config/zsh/plugins.zsh
@@ -4,7 +4,13 @@ plug "zsh-users/zsh-autosuggestions"
 plug "zap-zsh/supercharge"
 plug "zap-zsh/exa"
 
-plug "$HOME/.config/zsh-plugins/plugins/aliases"
-plug "$HOME/.config/zsh-plugins/plugins/brew"
-plug "$HOME/.config/zsh-plugins/plugins/direnv"
-plug "$HOME/.config/zsh-plugins/plugins/git"
+OMZ_PLUGINS=(
+  aliases
+  brew
+  direnv
+  git
+)
+
+for p in "${OMZ_PLUGINS[@]}"; do
+  plug "$ZDOTDIR/omz/plugins/$p"
+done
