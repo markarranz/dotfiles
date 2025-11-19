8cde12cde4f07b756b9ab228c44085ca81619836
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2025-11-19 12:03:52 -0800

    [sketchybar] Update shebang

--- private_dot_config/sketchybar/executable_colors.sh
+++ private_dot_config/sketchybar/executable_colors.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
-
-### Sonokai
-# export BLACK=0xff181819
-# export WHITE=0xffe2e2e3
-# export RED=0xfffc5d7c
-# export GREEN=0xff9ed072
-# export BLUE=0xff76cce0
-# export YELLOW=0xffe7c664
-# export ORANGE=0xfff39660
-# export MAGENTA=0xffb39df3
-# export GREY=0xff7f8490
-# export TRANSPARENT=0x00000000
-# export BG0=0xff2c2e34
-# export BG1=0xff363944
-# export BG2=0xff414550
+#!/usr/bin/env bash
 
 ### Catppuccin
 export BLACK=0xff181926
@@ -35,7 +20,7 @@ export BAR_COLOR=$BG0
 export BAR_BORDER_COLOR=$BG2
 export BACKGROUND_1=$BG1
 export BACKGROUND_2=$BG2
-export ICON_COLOR=$WHITE # Color of all icons
+export ICON_COLOR=$WHITE  # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
 export POPUP_BACKGROUND_COLOR=$BAR_COLOR
 export POPUP_BORDER_COLOR=$WHITE
