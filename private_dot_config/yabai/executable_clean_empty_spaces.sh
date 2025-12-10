6fafcbb18f386ae40d0838cd0ede8727a960c7db
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2025-12-10 14:04:34 -0800

    [yabai] Delay workspace cleanup for native fullscreen apps

--- private_dot_config/yabai/executable_clean_empty_spaces.sh
+++ private_dot_config/yabai/executable_clean_empty_spaces.sh
@@ -2,6 +2,8 @@
 
 yabai -m query --spaces --display |
   jq -re 'map(select(."is-native-fullscreen" == false)) | length > 1' &&
+  # wait for any native fullscreen apps to get added back to their original workspace
+  sleep 2 &&
   yabai -m query --spaces |
   jq -re 'map(select(."windows" == [] and ."has-focus" == false).index) | reverse | .[] ' |
     xargs -I % sh -c 'yabai -m space % --destroy'
