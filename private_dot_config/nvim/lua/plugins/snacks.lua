e6a9d2a4c0cf023d6e9893f4a4709821e1e1dd45
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2026-02-22 11:40:51 -0800

    [neovim] disable LazyVim term_nav keys for cross-mux navigation

--- private_dot_config/nvim/lua/plugins/snacks.lua
+++ private_dot_config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,18 @@
 return {
 	"folke/snacks.nvim",
 	opts = {
+		terminal = {
+			win = {
+				keys = {
+					-- Disable LazyVim's term_nav keys so our TermOpen keymaps
+					-- (which use navigate.lua for cross-mux navigation) take effect
+					nav_h = false,
+					nav_j = false,
+					nav_k = false,
+					nav_l = false,
+				},
+			},
+		},
 		picker = {
 			sources = {
 				explorer = {
