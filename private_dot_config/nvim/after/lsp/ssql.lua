c5955e4431f278722ab8845ead2f8f53df154f41
Mark Arranz <4390451+MarkArranz@users.noreply.github.com>
2026-03-12 15:39:30 -0700

    [nvim] disable formatting from ssql LSP

--- private_dot_config/nvim/after/lsp/ssql.lua
+++ private_dot_config/nvim/after/lsp/ssql.lua
@@ -3,4 +3,8 @@ return {
 	filetypes = { "sql" },
 	root_markers = { ".googlesql.json", ".git" },
 	settings = {},
+	on_attach = function(client)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+	end,
 }
